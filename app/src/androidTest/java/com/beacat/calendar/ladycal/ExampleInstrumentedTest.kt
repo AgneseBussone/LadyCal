8ee48382de9eaf09660f5770619543ce62ba9480
Agnese Bussone <agnese.bussone@gmail.com>
2026-07-26 19:09:52 +0200

    Translate test to kotlin - not final tests

--- app/src/androidTest/java/com/beacat/calendar/ladycal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/beacat/calendar/ladycal/ExampleInstrumentedTest.kt
@@ -1,99 +1,99 @@
-package com.beacat.calendar.ladycal;
+package com.beacat.calendar.ladycal
 
-import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import com.tyczj.extendedcalendarview.Period;
-import com.tyczj.extendedcalendarview.PeriodDatabase;
+import com.tyczj.extendedcalendarview.Period
+import com.tyczj.extendedcalendarview.PeriodDatabase
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import java.util.Calendar;
-import java.util.List;
+import java.util.Calendar
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert.assertEquals
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
 
-    private Context appContext;
-    private PeriodDatabase db;
+    private lateinit var appContext: Context
+    private lateinit var db: PeriodDatabase
 
     @Before
-    public void useAppContext() throws Exception {
+    @Throws(Exception::class)
+    fun useAppContext() {
         // Context of the app under test.
-        appContext = InstrumentationRegistry.getTargetContext();
-        db = PeriodDatabase.getInstance(appContext);
+        appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        db = PeriodDatabase.getInstance(appContext)
 
         // set lengths because in the mainActivity is done during the onCreate
         // if you don't do that, the add will insert wrong values
-        db.setCycleLength(28, true);
-        db.setPeriodLength(6, true);
+        db.setCycleLength(28, true)
+        db.setPeriodLength(6, true)
 
-        assertEquals("com.beacat.calendar.ladycal", appContext.getPackageName());
+        assertEquals("com.beacat.calendar.ladycal", appContext.packageName)
     }
 
     /* Probably not the best use for tests, but I need to insert a bunch of data
     *  without do it by hand
     */
     @Test
-    public void createDb() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun createDb() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
-        cal.set(2016, Calendar.SEPTEMBER, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.SEPTEMBER, 5);
-        long end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.SEPTEMBER, 2)
+        var start = cal.timeInMillis
+        cal.set(2016, Calendar.SEPTEMBER, 5)
+        var end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2016, Calendar.OCTOBER, 10);
-        start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.OCTOBER, 14);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.OCTOBER, 10)
+        start = cal.timeInMillis
+        cal.set(2016, Calendar.OCTOBER, 14)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2016, Calendar.NOVEMBER, 12);
-        start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.NOVEMBER, 20);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.NOVEMBER, 12)
+        start = cal.timeInMillis
+        cal.set(2016, Calendar.NOVEMBER, 20)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2016, Calendar.DECEMBER, 25);
-        start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.DECEMBER, 30);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.DECEMBER, 25)
+        start = cal.timeInMillis
+        cal.set(2016, Calendar.DECEMBER, 30)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2017, Calendar.JANUARY, 9);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.JANUARY, 15);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.JANUARY, 9)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.JANUARY, 15)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2017, Calendar.JANUARY, 29);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.FEBRUARY, 4);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.JANUARY, 29)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.FEBRUARY, 4)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        cal.set(2017, Calendar.APRIL, 16);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.APRIL, 20);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.APRIL, 16)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.APRIL, 20)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
 
-        List<Period> list = db.getAllPeriods("DESC");
-        assertEquals(list.size(), 7);
-
+        val list = db.getAllPeriods("DESC")
+        assertEquals(list!!.size.toLong(), 7)
     }
 }
--- app/src/test/java/com/beacat/calendar/ladycal/DatabaseTest.kt
+++ app/src/test/java/com/beacat/calendar/ladycal/DatabaseTest.kt
@@ -1,149 +1,152 @@
-package com.beacat.calendar.ladycal;
+package com.beacat.calendar.ladycal
 
-import android.content.Context;
+import android.content.Context
 
-import com.tyczj.extendedcalendarview.Day;
-import com.tyczj.extendedcalendarview.Period;
-import com.tyczj.extendedcalendarview.PeriodDatabase;
+import com.tyczj.extendedcalendarview.Day
+import com.tyczj.extendedcalendarview.Period
+import com.tyczj.extendedcalendarview.PeriodDatabase
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
-import java.util.Calendar;
-import java.util.List;
+import java.util.Calendar
 
-import static org.junit.Assert.*;
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotSame
 
 /**
  * Testing the database functionality
  */
-@RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 23, manifest = "/src/main/AndroidManifest.xml")
-public class DatabaseTest{
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = [23], manifest = "/src/main/AndroidManifest.xml")
+class DatabaseTest {
 
-    private Context context;
-    private PeriodDatabase db;
+    private lateinit var context: Context
+    private lateinit var db: PeriodDatabase
 
     @Before
-    public void setup(){
-        context = RuntimeEnvironment.application;
-        db = PeriodDatabase.getInstance(context);
-        db.setCycleLength(28, true);
-        db.setPeriodLength(6, true);
+    fun setup() {
+        context = RuntimeEnvironment.application
+        db = PeriodDatabase.getInstance(context)
+        db.setCycleLength(28, true)
+        db.setPeriodLength(6, true)
     }
 
     @Test
-    public void test_addPeriod() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun test_addPeriod() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
-        cal.set(2016, Calendar.NOVEMBER, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.NOVEMBER, 5);
-        long end = cal.getTimeInMillis();
+        cal.set(2016, Calendar.NOVEMBER, 2)
+        val start = cal.timeInMillis
+        cal.set(2016, Calendar.NOVEMBER, 5)
+        val end = cal.timeInMillis
 
-        db.addPeriod(new Period(start, end));
-        List<Period> list = db.getAllPeriods("DESC");
-        assertEquals(list.size(), 1);
+        db.addPeriod(Period(start, end))
+        var list = db.getAllPeriods("DESC")
+        assertEquals(list!!.size.toLong(), 1)
 
         // Re-insert the same
-        db.addPeriod(new Period(start, end));
-        list = db.getAllPeriods("DESC");
+        db.addPeriod(Period(start, end))
+        list = db.getAllPeriods("DESC")
 
-        assertEquals(1, list.size()); // expect to have only 1 entry
+        assertEquals(1, list!!.size.toLong()) // expect to have only 1 entry
 
-        Period p = list.get(0);
-        assertEquals(start, p.getStartDay());
-        assertEquals(end, p.getEndDay());
-        assertEquals(4, p.getPeriodLength());
-        assertEquals(28, p.getCycleLength());
+        val p = list[0]
+        assertEquals(start, p.startDay)
+        assertEquals(end, p.endDay)
+        assertEquals(4, p.periodLength)
+        assertEquals(28, p.cycleLength)
     }
 
     @Test
-    public void test_lengthValues() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun test_lengthValues() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
         // period #1
-        cal.set(2017, Calendar.JANUARY, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.JANUARY, 5);
-        long end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.JANUARY, 2)
+        var start = cal.timeInMillis
+        cal.set(2017, Calendar.JANUARY, 5)
+        var end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
         // period #2
-        cal.set(2017, Calendar.MARCH, 10);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.MARCH, 20);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.MARCH, 10)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.MARCH, 20)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
         // period #3 - history insertion. This should update itself and january
-        cal.set(2017, Calendar.FEBRUARY, 4);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.FEBRUARY, 5);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.FEBRUARY, 4)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.FEBRUARY, 5)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
 
-        List<Period> list = db.getAllPeriods("DESC");
+        val list = db.getAllPeriods("DESC")
 
-        Period p = list.get(0); //March
-        assertEquals(10, p.getPeriodLength());
-        assertEquals(28, p.getCycleLength());
+        var p = list!![0] //March
+        assertEquals(10, p.periodLength)
+        assertEquals(28, p.cycleLength)
 
-        p = list.get(1); //February
-        assertEquals(2, p.getPeriodLength());
-        assertEquals(34, p.getCycleLength());
+        p = list[1] //February
+        assertEquals(2, p.periodLength)
+        assertEquals(34, p.cycleLength)
 
-        p = list.get(2); //January
-        assertEquals(4, p.getPeriodLength());
-        assertEquals(33, p.getCycleLength());
+        p = list[2] //January
+        assertEquals(4, p.periodLength)
+        assertEquals(33, p.cycleLength)
     }
 
     @Test
-    public void test_updatePeriod() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun test_updatePeriod() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
         // period #1
-        cal.set(2017, Calendar.JANUARY, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.JANUARY, 5);
-        long end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.JANUARY, 2)
+        var start = cal.timeInMillis
+        cal.set(2017, Calendar.JANUARY, 5)
+        var end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
         // period #2
-        cal.set(2017, Calendar.FEBRUARY, 4);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.FEBRUARY, 5);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.FEBRUARY, 4)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.FEBRUARY, 5)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
         // period #3
-        cal.set(2017, Calendar.MARCH, 10);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.MARCH, 20);
-        end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2017, Calendar.MARCH, 10)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.MARCH, 20)
+        end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        List<Period> list = db.getAllPeriods("DESC");
+        var list = db.getAllPeriods("DESC")
 
-        Period p = list.get(0); //march
-        assertEquals(10, p.getPeriodLength());
-        assertEquals(28, p.getCycleLength());
+        var p = list!![0] //march
+        assertEquals(10, p.periodLength)
+        assertEquals(28, p.cycleLength)
 
-        p = list.get(1); //february
-        assertEquals(2, p.getPeriodLength());
-        assertEquals(34, p.getCycleLength());
+        p = list[1] //february
+        assertEquals(2, p.periodLength)
+        assertEquals(34, p.cycleLength)
 
-        p = list.get(2); //january
-        assertEquals(4, p.getPeriodLength());
-        assertEquals(33, p.getCycleLength());
+        p = list[2] //january
+        assertEquals(4, p.periodLength)
+        assertEquals(33, p.cycleLength)
 /*
         // update march
         cal.set(2017, Calendar.MARCH, 5);
@@ -191,102 +194,105 @@ public class DatabaseTest{
         assertEquals(34, p.getCycleLength());
 */
         // update february
-        cal.set(2017, Calendar.FEBRUARY, 10);
-        start = cal.getTimeInMillis();
-        cal.set(2017, Calendar.FEBRUARY, 15);
-        end = cal.getTimeInMillis();
-        db.updatePeriod(list.get(1), new Period(start, end));
+        cal.set(2017, Calendar.FEBRUARY, 10)
+        start = cal.timeInMillis
+        cal.set(2017, Calendar.FEBRUARY, 15)
+        end = cal.timeInMillis
+        db.updatePeriod(list[1], Period(start, end))
 
-        list.clear();
-        list = db.getAllPeriods("DESC");
+        list!!.clear()
+        list = db.getAllPeriods("DESC")
 
-        p = list.get(0); //march
-        assertEquals(10, p.getPeriodLength());
-        assertEquals(28, p.getCycleLength());
+        p = list!![0] //march
+        assertEquals(10, p.periodLength)
+        assertEquals(28, p.cycleLength)
 
-        p = list.get(1); //february
-        assertEquals(6, p.getPeriodLength());
-        assertEquals(28, p.getCycleLength());
+        p = list[1] //february
+        assertEquals(6, p.periodLength)
+        assertEquals(28, p.cycleLength)
 
-        p = list.get(2); //january
-        assertEquals(4, p.getPeriodLength());
-        assertEquals(39, p.getCycleLength());
-
-    }
-
-    @Test
-    public void test_deletePeriod() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
-
-        cal.set(2016, Calendar.NOVEMBER, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.NOVEMBER, 5);
-        long end = cal.getTimeInMillis();
-
-        Period p = new Period(start, end);
-        db.addPeriod(p);
-        List<Period> list = db.getAllPeriods("DESC");
-        assertEquals(1, list.size());
-
-        db.deletePeriod(p);
-
-        list = db.getAllPeriods("DESC");
-        assertEquals(0, list.size());
+        p = list[2] //january
+        assertEquals(4, p.periodLength)
+        assertEquals(39, p.cycleLength)
 
     }
 
     @Test
-    public void test_searchPeriod() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun test_deletePeriod() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
-        cal.set(2016, Calendar.NOVEMBER, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.NOVEMBER, 5);
-        long end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.NOVEMBER, 2)
+        val start = cal.timeInMillis
+        cal.set(2016, Calendar.NOVEMBER, 5)
+        val end = cal.timeInMillis
 
-        cal.set(2016, Calendar.JANUARY, 2);
-        long start1 = cal.getTimeInMillis();
-        cal.set(2016, Calendar.JANUARY, 5);
-        long end1 = cal.getTimeInMillis();
-        db.addPeriod(new Period(start1, end1));
+        val p = Period(start, end)
+        db.addPeriod(p)
+        var list = db.getAllPeriods("DESC")
+        assertEquals(1, list!!.size.toLong())
 
-        cal.set(2016, Calendar.JANUARY, 2);
-        long start2 = cal.getTimeInMillis();
-        cal.set(2016, Calendar.JANUARY, 5);
-        long end2 = cal.getTimeInMillis();
-        db.addPeriod(new Period(start2, end2));
+        db.deletePeriod(p)
 
-        Period p = new Period(start, end);
-        assertNotSame(-1, db.searchPeriodId(p));  // expected to exist
+        list = db.getAllPeriods("DESC")
+        assertEquals(0, list!!.size.toLong())
 
-        Period p1 = new Period(50, 50);
-        assertEquals(-1, db.searchPeriodId(p1));  // expected to not exist
     }
 
     @Test
-    public void test_isPeriod() throws Exception{
-        Calendar cal = Calendar.getInstance();
-        cal.clear();
+    @Throws(Exception::class)
+    fun test_searchPeriod() {
+        val cal = Calendar.getInstance()
+        cal.clear()
 
-        cal.set(2016, Calendar.NOVEMBER, 2);
-        long start = cal.getTimeInMillis();
-        cal.set(2016, Calendar.NOVEMBER, 10);
-        long end = cal.getTimeInMillis();
-        db.addPeriod(new Period(start, end));
+        cal.set(2016, Calendar.NOVEMBER, 2)
+        val start = cal.timeInMillis
+        cal.set(2016, Calendar.NOVEMBER, 5)
+        val end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
 
-        Day d = new Day(context, 2016, Calendar.NOVEMBER, 4);
-        assertNotSame(-1, db.isPeriod(d)); // Expected to be in the period
+        cal.set(2016, Calendar.JANUARY, 2)
+        val start1 = cal.timeInMillis
+        cal.set(2016, Calendar.JANUARY, 5)
+        val end1 = cal.timeInMillis
+        db.addPeriod(Period(start1, end1))
 
-        Day d1 = new Day(context, 2016, Calendar.NOVEMBER, 2);
-        assertNotSame(-1, db.isPeriod(d1)); // Expected to be in the period
+        cal.set(2016, Calendar.JANUARY, 2)
+        val start2 = cal.timeInMillis
+        cal.set(2016, Calendar.JANUARY, 5)
+        val end2 = cal.timeInMillis
+        db.addPeriod(Period(start2, end2))
 
-        Day d2 = new Day(context, 2016, Calendar.NOVEMBER, 10);
-        assertNotSame(-1, db.isPeriod(d2)); // Expected to be in the period
+        val p = Period(start, end)
+        assertNotSame(-1, db.searchPeriodId(p))  // expected to exist
 
-        Day d3 = new Day(context, 2016, Calendar.DECEMBER, 18);
-        assertEquals(-1, db.isPeriod(d3)); // Expected NOT to be in the period
+        val p1 = Period(50, 50)
+        assertEquals(-1, db.searchPeriodId(p1))  // expected to not exist
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun test_isPeriod() {
+        val cal = Calendar.getInstance()
+        cal.clear()
+
+        cal.set(2016, Calendar.NOVEMBER, 2)
+        val start = cal.timeInMillis
+        cal.set(2016, Calendar.NOVEMBER, 10)
+        val end = cal.timeInMillis
+        db.addPeriod(Period(start, end))
+
+        val d = Day(context, 2016, Calendar.NOVEMBER, 4)
+        assertNotSame(-1, db.isPeriod(d)) // Expected to be in the period
+
+        val d1 = Day(context, 2016, Calendar.NOVEMBER, 2)
+        assertNotSame(-1, db.isPeriod(d1)) // Expected to be in the period
+
+        val d2 = Day(context, 2016, Calendar.NOVEMBER, 10)
+        assertNotSame(-1, db.isPeriod(d2)) // Expected to be in the period
+
+        val d3 = Day(context, 2016, Calendar.DECEMBER, 18)
+        assertEquals(-1, db.isPeriod(d3)) // Expected NOT to be in the period
     }
 }
